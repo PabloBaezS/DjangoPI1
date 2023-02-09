50c5f2f427cf07e904bbc66fce1b48062e5f87a4
Pablo Baez Santamaria <pbaezs@eafit.edu.co>
2023-02-09 11:04:15 -0500

    archivo html

--- practiceApp/views.py
+++ practiceApp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
     #return HttpResponse('<h1> Welcome to Home Page</h1>')
     #return render(request, 'home.html')
